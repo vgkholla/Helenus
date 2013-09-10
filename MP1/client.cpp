246f70afe3226d25be133317f8d63db5bb5e9bb7
vgkholla <vgkholla@gmail.com>
2013-09-10 17:33:01 -0500

    lient shows prompt

--- MP1/client.cpp
+++ MP1/client.cpp
@@ -8,11 +8,14 @@
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <unistd.h>
+#include "headers/clt.h"
 
-int main(int argv, char** argc){
+using namespace std;
+
+int main(){
 
     int host_port= 45456;
-    char* host_name="127.0.0.1";
+    string host_name="127.0.0.1";
 
     struct sockaddr_in my_addr;
 
@@ -21,13 +24,13 @@ int main(int argv, char** argc){
     int buffer_len=0;
 
     int hsock;
-    int * p_int;
+    int *p_int;
     int err;
 
     hsock = socket(AF_INET, SOCK_STREAM, 0);
     if(hsock == -1){
         printf("Error initializing socket %d\n",errno);
-        goto FINISH;
+        exit(1);
     }
     
     p_int = (int*)malloc(sizeof(int));
@@ -37,7 +40,7 @@ int main(int argv, char** argc){
         (setsockopt(hsock, SOL_SOCKET, SO_KEEPALIVE, (char*)p_int, sizeof(int)) == -1 ) ){
         printf("Error setting options %d\n",errno);
         free(p_int);
-        goto FINISH;
+        exit(1);
     }
     free(p_int);
 
@@ -45,12 +48,12 @@ int main(int argv, char** argc){
     my_addr.sin_port = htons(host_port);
     
     memset(&(my_addr.sin_zero), 0, 8);
-    my_addr.sin_addr.s_addr = inet_addr(host_name);
+    my_addr.sin_addr.s_addr = inet_addr(host_name.c_str());
 
     if( connect( hsock, (struct sockaddr*)&my_addr, sizeof(my_addr)) == -1 ){
         if((err = errno) != EINPROGRESS){
             fprintf(stderr, "Error connecting socket %s\n", strerror(errno));
-            goto FINISH;
+            exit(1);
         }
     }
 
@@ -61,24 +64,22 @@ int main(int argv, char** argc){
     memset(buffer, '\0', buffer_len);
 
     printf("Enter some text to send to the server (press enter)\n");
+    string cmd = CommandLineTools::showAndHandlePrompt(1);
     //fgets(buffer, 1024, stdin);
     memcpy(buffer,"client",6);
     buffer[strlen(buffer)]='\0';
     
     if( (bytecount=send(hsock, buffer, strlen(buffer),0))== -1){
         fprintf(stderr, "Error sending data %d\n", errno);
-        goto FINISH;
+        exit(1);
     }
     printf("Sent bytes %d\n", bytecount);
 
     if((bytecount = recv(hsock, buffer, buffer_len, 0))== -1){
         fprintf(stderr, "Error receiving data %d\n", errno);
-        goto FINISH;
+        exit(1);
     }
     printf("Recieved bytes %d\nReceived string \"%s\"\n", bytecount, buffer);
 
     close(hsock);
-    
-FINISH:
-;
 }
